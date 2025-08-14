7d3e6e774dc38cd43805355451cb860fdcbf8fe6
tungnk123 <doanthanhtungnk123@gmail.com>
2025-08-14 21:30:17 +0700

    Update main function to use configuration in code

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,7 +4,10 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>) = EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
 
 fun Application.module() {
     configureHTTP()
